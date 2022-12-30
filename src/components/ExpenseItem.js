daeb72acb86f7d04fe2a42081b45cac06f20ae2c
Nitin <nitinkhokher999@gmail.com>
2022-12-30 18:16:47 +0530

    Created ExpenseDate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,17 @@
+import ExpenseDate from "./ExpenseDate";  
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    return (
-        <div className="expense-item">
-            <div>December 29th 2022</div>
-            <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$250</div>
-            </div>
-        </div>
-    );
+function ExpenseItem(props) {
+  
+  return (
+    <div className="expense-item">
+      <ExpenseDate date = {props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div> 
+    </div>
+  );
 }
 
 export default ExpenseItem;

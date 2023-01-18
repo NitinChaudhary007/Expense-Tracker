13dac5454301acf8055e50d25dbef049c543ef40
Nitin <nitinkhokher999@gmail.com>
2023-01-18 23:49:34 +0530

    Modified NewExpenses

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -26,7 +26,8 @@ const ExpenseForm = (props) => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    // console.log(props.onSaveExpenseData);
+    
+
     props.onSaveExpenseData(expenseData);
 
     setEnteredTitle("");
@@ -67,6 +68,7 @@ const ExpenseForm = (props) => {
           />
         </div>
         <div className="new-expense__actions">
+          <button type="button" onClick={props.onCancel}>Cancel</button>
           <button type="submit">Add Expense</button>
         </div>
       </div>

1bddf2bb1207b4f40927bd1d4b0ef32c91560670
Nitin <nitinkhokher999@gmail.com>
2023-01-18 21:12:13 +0530

    Modified Expenses.js component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,66 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
+
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-
+  
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
+  // 3 METHOD-> Using a variable to store JSX 
+  
+  
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
+      <ExpensesList items={filteredExpenses} />
+      
+  {/* OUTPUTTING CONDITIONAL CONTENT */}
+    {/*1 METHOD-> Using JS ternary operator to output conditonal contents */}
+      {/* {filteredExpenses.length ===0 ? <p>No Expenses Found</p> : filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
         />
-      ))};
+      ))} */}
+
+    {/*2 METHOD-> Abusing a little trick in JS ...That is -> If the first statement is true then JS returns the statement after the &&*/}
+      {/* {filteredExpenses.length ===0 && <p>No Expenses Found</p>}
+      {filteredExpenses.length >0 && filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))} */}
+
+
+    {/* Displays filteredExpenses Array for a particular year. If Array is empty display nothing  */}
+      {/* {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))} */}
+
     </Card>
   );
 };
